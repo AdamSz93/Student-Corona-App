3b0afd3c237f07a66bca6b4dcdc8e2efdc634244
KohlmannJens <32839892+KohlmannJens@users.noreply.github.com>
2020-05-06 22:37:29 +0200

    showHelpers
    Tabelle mit Helfern und den GA-Informationen (Darstellung ist noch offen - bisher mit Zahlen gefüllt) sowie einem Button zum Anschreiben (bisher als Platzhalter)

--- src/views/ShowHelpers.vue
+++ src/views/ShowHelpers.vue
@@ -1,5 +1,72 @@
 <template>
-    <div>
-        <h1>Show helpers</h1>
-    </div>
+  <div>
+    <h1>Show helpers</h1>
+    <v-card>
+      <v-card-title>
+        Helfer
+        <v-spacer></v-spacer>
+        <v-text-field
+          v-model="search"
+          append-icon="mdi-magnify"
+          label="Search"
+          single-line
+          hide-details
+        ></v-text-field>
+      </v-card-title>
+      <v-data-table :headers="headers" :items="helpers" :search="search">
+            <template v-slot:item.sendMessage="{item}">
+                <v-icon small class="mr-2" @click="update(item)">
+                    mdi-send
+                </v-icon>
+            </template>
+      </v-data-table>
+    </v-card>
+  </div>
 </template>
+
+<script>
+export default {
+  name: "ShowHelpers",
+
+  components: {},
+  methods: {
+      update:function(i){ 
+          console.log("es funzt!" + i)
+  }
+  },
+  data: () => {
+    return {
+        search: '',
+        headers: [
+          {
+            text: 'Helfer', align: 'start', sortable: false, value: 'name',
+          },
+          { text: 'Wissensstand', value: 'wissensstand' },
+          { text: 'Freundlichkeit', value: 'freundlichkeit' },
+          { text: ' durchschnittliche Antwortzeit', value: 'antwortzeit' },
+          { text: '', value: 'sendMessage', sortable: false},
+        ],
+        helpers: [
+          {
+            name: 'Mathilda',
+            wissensstand: 3.5,
+            freundlichkeit: 4.0,
+            antwortzeit: 2.5, 
+          },
+          {
+            name: 'Leon',
+            wissensstand: 4.0,
+            freundlichkeit: 2.0,
+            antwortzeit: 2.0,
+          },
+                    {
+            name: 'Simone',
+            wissensstand: 5.0,
+            freundlichkeit: 3.0,
+            antwortzeit: 4.0,
+          },
+        ],
+      }
+    },
+  }
+</script>
