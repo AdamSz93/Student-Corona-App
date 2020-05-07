6eeeda8120dd859dc1d060e5d2515ac8255a5531
Christian Schlusche <christian@schlusche.net>
2020-05-07 18:00:58 +0200

    Navigation: Rudimentary nested Lists for requests

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,55 @@
 <template>
   <v-app>
-    <v-navigation-drawer app>
+    <v-navigation-drawer app permanent>
       <v-list dense nav>
-        <router-link to="/questionmanager">Fragen &amp; Antworten</router-link>
+        <v-list-item>
+          <v-list-item-avatar>
+            <img src="https://randomuser.me/api/portraits/women/11.jpg" />
+          </v-list-item-avatar>
+          <v-list-item-content>
+            <v-list-item-title>@MonaLisa</v-list-item-title>
+            <v-list-item-subtitle>Sommersemester 2020</v-list-item-subtitle>
+          </v-list-item-content>
+        </v-list-item>
+
         <v-divider></v-divider>
+
+        <v-list-item>
+          <v-list-item-icon>
+            <v-icon>mdi-comment-question</v-icon>
+          </v-list-item-icon>
+          <v-list-item-content>
+            <v-list-item-title>
+              <router-link to="/questionmanager">Fragen &amp; Antworten</router-link>
+            </v-list-item-title>
+          </v-list-item-content>
+        </v-list-item>
+
+        <v-list-item>
+          <v-list-item-content>Anfragen an mich:</v-list-item-content>
+        </v-list-item>
+        
+        <v-list-group value="true">
+          <template v-slot:activator>
+            <v-list-item-title>Psychologie A</v-list-item-title>
+          </template>
+
+            <v-list-item>Präfrontalcortex bei Suchtverhalten?</v-list-item>
+            <v-list-item>Amygdala für Impulskontrolle?</v-list-item>          
+        </v-list-group>
+
+        <v-divider></v-divider>
+
+        <v-list-item>
+          <v-list-item-content>Meine offenen Fragen:</v-list-item-content>
+        </v-list-item>
+
+        <v-divider></v-divider>
+
+        <v-list-item>
+          <v-list-item-content>Meine Gruppen:</v-list-item-content>
+        </v-list-item>
+
         <v-list-item v-for="group in groups" :key="group.name" link>
           <v-list-item-icon>
             <v-icon>{{ group.icon }}</v-icon>
@@ -19,13 +65,22 @@
     </v-navigation-drawer>
 
     <v-app-bar app dark>
-      
       <v-tabs>
-        <v-tab><router-link to="/home">Home</router-link></v-tab>
-        <v-tab><router-link to="/profile">Profil</router-link></v-tab>
-        <v-tab><router-link to="/settings">xxxEinstellungen</router-link></v-tab>
-        <v-tab><router-link to="/help">xxxHilfe</router-link></v-tab>
-        <v-tab><router-link to="/logout">xxxLogout</router-link></v-tab>
+        <v-tab>
+          <router-link to="/home">Home</router-link>
+        </v-tab>
+        <v-tab>
+          <router-link to="/profile">Profil</router-link>
+        </v-tab>
+        <v-tab>
+          <router-link to="/settings">xxxEinstellungen</router-link>
+        </v-tab>
+        <v-tab>
+          <router-link to="/help">xxxHilfe</router-link>
+        </v-tab>
+        <v-tab>
+          <router-link to="/logout">xxxLogout</router-link>
+        </v-tab>
       </v-tabs>
     </v-app-bar>
 
@@ -37,7 +92,7 @@
         <router-view></router-view>
       </v-container>
 
-     <plusbutton msg="ggg"/>   
+      <plusbutton msg="ggg" />
     </v-content>
 
     <v-footer app>
@@ -48,7 +103,6 @@
 
 <script>
 import plusbutton from "@/components/plusbutton.vue";
-
 
 export default {
   name: "App",
